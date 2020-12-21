d9fae0168f1791c88597de4ecd3e6bf40ff00b0e
My Lord <page.c.tyler@gmail.com>
2020-12-20 23:08:31 -0500

    database connection established.  loading documents success.

--- src/main/scala/KeplerDataReader/Planet.scala
+++ src/main/scala/KeplerDataReader/Planet.scala
@@ -2,8 +2,7 @@ package KeplerDataReader
 
 import org.bson.types.ObjectId
 
-
-case class Planet() {
+case class Planet(_id: ObjectId) {
   var name: String = ""
   var hostStar: String = ""
   var yearDiscovered: Int = 0
@@ -17,124 +16,11 @@ case class Planet() {
 
   override def toString = {
     s"Planet: $name | Host Star: $hostStar | Discovery Year $yearDiscovered | Orbital Period: $orbitalPeriod | Radius: $radius |" +
-    s" Mass: $mass | Temperature: $eqTemp | Stellar Mass: $stellarMass | Stellar Radius: $stellarRadius | Distance: $distance"
+      s" Mass: $mass | Temperature: $eqTemp | Stellar Mass: $stellarMass | Stellar Radius: $stellarRadius | Distance: $distance"
   }
 }
 
 object Planet {
-  def apply(name: String, hostStar: String): Planet = {
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    p
-  }
-  def apply(name: String, hostStar: String, col: Column): Planet = {
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    this.setFieldValues(p, col)
-    p
-  }
-  def apply(name: String, hostStar: String, col3: Column, col4: Column): Planet = {
-    val cols = List(col3, col4)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-  def apply(name: String, hostStar: String, col3: Column, col4: Column, col5: Column): Planet = {
-    val cols = List(col3, col4, col5)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-  def apply(
-             name: String, hostStar: String, col3: Column, col4: Column, col5: Column, col6: Column
-           ): Planet = {
-    val cols = List(col3, col4, col5, col6)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-  def apply(
-             name: String, hostStar: String, col3: Column, col4: Column, col5: Column, col6: Column, col7: Column
-           ): Planet = {
-    val cols = List(col3, col4, col5, col6, col7)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-  def apply(
-             name: String,
-             hostStar: String,
-             col3: Column,
-             col4: Column,
-             col5: Column,
-             col6: Column,
-             col7: Column,
-             col8: Column
-           ): Planet = {
-    val cols = List(col3, col4, col5, col6, col7, col8)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-  def apply(
-             name: String,
-             hostStar: String,
-             col3: Column,
-             col4: Column,
-             col5: Column,
-             col6: Column,
-             col7: Column,
-             col8: Column,
-             col9: Column
-           ): Planet = {
-    val cols = List(col3, col4, col5, col6, col7, col8, col9)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-  def apply(
-             name: String,
-             hostStar: String,
-             col3: Column,
-             col4: Column,
-             col5: Column,
-             col6: Column,
-             col7: Column,
-             col8: Column,
-             col9: Column,
-             col10: Column
-           ): Planet = {
-    val cols = List(col3, col4, col5, col6, col7, col8, col9, col10)
-    var p = new Planet
-    p.name = name
-    p.hostStar = hostStar
-    for (col <- cols) this.setFieldValues(p, col)
-    p
-  }
-
-  private def setFieldValues(p: Planet, col: Column): Unit = col match {
-      case c: DiscoverYear => p.yearDiscovered = c.value
-      case c: OrbitalPeriod => p.orbitalPeriod = c.value
-      case c: StellarRadius => p.stellarRadius = c.value
-      case c: StellarMass => p.stellarMass = c.value
-      case c: Temperature => p.eqTemp = c.value
-      case c: Distance => p.distance = c.value
-      case c: Radius => p.radius = c.value
-      case c: Mass => p.mass = c.value
-  }
+  def apply(): Planet = new Planet(new ObjectId())
 }
 
--- src/main/scala/KeplerDataReader/PlanetDAO.scala
+++ src/main/scala/KeplerDataReader/PlanetDAO.scala
@@ -1,31 +1,25 @@
 package KeplerDataReader
 
 import org.mongodb.scala._
-
-import scala.collection.mutable.{ArrayBuffer, Map}
+import org.mongodb.scala.bson.codecs.Macros._
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.{Duration, SECONDS}
 import org.mongodb.scala.model.Filters.equal
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 
-import scala.reflect.internal.util.TableDef.Column
-
-
-
-
 class PlanetDAO(mongoClient: MongoClient) {
-//  val codecRegistry = fromRegistries(
-//    fromProviders(classOf[Planet]),
-//    MongoClient.DEFAULT_CODEC_REGISTRY
-//  )
-  val db = mongoClient.getDatabase("kepler_db")
+  val codecRegistry = fromRegistries(
+    fromProviders(classOf[Planet]),
+    MongoClient.DEFAULT_CODEC_REGISTRY
+  )
+  val db: MongoDatabase = mongoClient.getDatabase("kepler_db").withCodecRegistry(codecRegistry)
   val collection: MongoCollection[Planet] = db.getCollection("exoplanets")
 
   private def getResults[T](obs: Observable[T]): Seq[T] = {
     Await.result(obs.toFuture(), Duration(10, SECONDS))
   }
 
-  def getAll(): Seq[Any] = getResults(collection.find())
+  def getAll: Seq[Any] = getResults(collection.find())
 
   def getByName(name: String): Seq[Any] = {
     getResults(collection.find(equal("name", name)))
@@ -34,30 +28,28 @@ class PlanetDAO(mongoClient: MongoClient) {
   def createNewCollection(table: List[String]): Unit = {
     val header = table.head.split(",")
     val docs = table.tail.map(row => (header zip row.split(",")).toMap)
-    var planets = ArrayBuffer[Planet]()
 
-    docs.foreach(m => {
-      val p = new Planet()
-      for ((k,v) <- m) addNeededColumns(k, v, p)
-      println(p)
-    })
-//    try {
-//      m.foreach(doc => {
-//        val planet = Planet(doc.map((k,v) => )
-//      })
-//      collection.insertMany(m.map(doc => doc.map(entry => Document(entry))))
+    try {
+      docs.foreach(m => {
+        val p = Planet()
+        for ((k, v) <- m) getColumnValues(k, v, p)
+        getResults(collection.insertOne(p))
+      })
+    } catch {
+      case e: Throwable => println(e)
     }
+  }
 
-  private def addNeededColumns(key: String, value: String, p: Planet): Unit = key match {
+  private def getColumnValues(key: String, value: String, p: Planet): Unit = key match {
     case "planet" => p.name = value
     case "host_star" => p.hostStar = value
-    case "discovery_year" => p.yearDiscovered = if(value == "") 0 else value.toInt
-    case "orbital_Period_(days)" => p.orbitalPeriod = if(value == "") 0 else value.toDouble
-    case "radius_(earth r)" => p.radius = if(value == "") 0 else value.toDouble
-    case "mass_(earth m)" => p.mass = if(value == "") 0 else value.toDouble
-    case "eq_temp_(K)" => p.eqTemp = if(value == "") 0 else value.toDouble
-    case "stellar_radius_(solar r)" => if(value == "") 0 else p.stellarRadius = value.toDouble
-    case "stellar_mass_(solar m)" => if(value == "") 0 else p.stellarMass = value.toDouble
-    case "distance_(pc)" => if(value == "") 0 else p.distance = value.toDouble
+    case "discovery_year" => p.yearDiscovered = if (value == "") return else value.toInt
+    case "orbital_Period_(days)" => p.orbitalPeriod = if (value == "") return else value.toDouble
+    case "radius_(earth r)" => p.radius = if (value == "") return else value.toDouble
+    case "mass_(earth m)" => p.mass = if (value == "") return else value.toDouble
+    case "eq_temp_(K)" => p.eqTemp = if (value == "") return else value.toDouble
+    case "stellar_radius_(solar r)" => p.stellarRadius = if (value == "") return else value.toDouble
+    case "stellar_mass_(solar m)" => p.stellarMass = if (value == "") return else value.toDouble
+    case "distance_(pc)" => p.distance = if (value == "") return else value.toDouble
   }
 }
